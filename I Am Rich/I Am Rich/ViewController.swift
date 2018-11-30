3351d04b0f2562c18b4f5f55605e813cafa8ce0f
JD <juandiegogarita@gmail.com>
2018-11-29 22:13:48 -0600

    init project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by JuanDiego Garita on 11/29/18.
+//  Copyright © 2018 JuanDiego Garita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
